2dbf825b7290abf117c2fe1ac2b8ef661f974ee4
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-08-24 13:26:44 -0700

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,13 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+
+    attr_accessor :difference
+
+    def initialize(number)
+        @difference = sum_square_difference(number)
+    end
+
+    def sum_square_difference(number)
+        (1..number).sum**2 - (1..number).sum{|number| number**2}
+    end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,3 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+    (1..number).sum**2 - (1..number).sum{|number| number**2}
+end
